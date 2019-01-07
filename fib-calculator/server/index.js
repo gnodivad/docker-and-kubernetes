e453e661b78ca4a107fb5d758d860ae095091c4f
David <dodwei.dev@gmail.com>
2019-01-07 09:34:25 +0800

    Connect to PostgreSQL

---
+++ fib-calculator/server/index.js
@@ -0,0 +1,22 @@
+const keys = require('./keys');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
