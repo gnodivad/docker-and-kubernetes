2494897178af33f963d06bac80e2ee38c4e2c16e
David <dodwei.dev@gmail.com>
2019-01-11 14:54:43 +0800

    Add render method to Fib Component

--- fib-calculator/client/src/Fib.js
+++ fib-calculator/client/src/Fib.js
@@ -24,4 +24,54 @@ class Fib extends Component {
             seenIndexes: seenIndexes.data
         });
     }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        await axios.post("/api/values", {
+            index: this.state.index
+        });
+        this.setState({ index: "" });
+    };
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number }) => number).join(", ");
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for (const key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculate {this.state.values[key]}
+                </div>
+            );
+        }
+
+        return entries;
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label for="">Enter you index:</label>
+                    <input
+                        value={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value })}
+                    />
+                    <button>Submit</button>
+                </form>
+
+                <h3>Indexes I have seen:</h3>
+                {this.renderSeenIndexes()}
+
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
+            </div>
+        );
+    }
 }
+
+export default Fib;
