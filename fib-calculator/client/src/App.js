65482dc534868f20b33241c76aadfd2ec7053318
David <dodwei.dev@gmail.com>
2019-02-23 11:47:37 +0800

    Remove logo from main page

--- fib-calculator/client/src/App.js
+++ fib-calculator/client/src/App.js
@@ -11,15 +11,6 @@ class App extends Component {
             <Router>
                 <div className="App">
                     <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
                         <Link to="/">Home</Link>
                         <Link to="/otherpage">Other Page</Link>
                     </header>
